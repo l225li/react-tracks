e71823d256622b16078cdb894792007873eade26
l225li <vanessa.li104@gmail.com>
2019-05-16 16:16:54 +0200

    created tracks app and updated Track model

---
+++ app/tracks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Track(models.Model):
+    # id
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
