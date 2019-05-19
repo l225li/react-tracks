f055ebd93969aafe26cd39910930a65d4de65e6b
l225li <vanessa.li104@gmail.com>
2019-05-19 18:28:29 +0200

    added handleSignout

--- react-tracks-client/src/components/Auth/Signout.js
+++ react-tracks-client/src/components/Auth/Signout.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { ApolloConsumer } from 'react-apollo'
 import withStyles from "@material-ui/core/styles/withStyles";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
 const Signout = ({ classes }) => {
+  const handleSignout = client => {
+    localStorage.removeItem('authToken')
+    client.writeData({ data: {isLoggedId: false }})
+    console.log('Signed out user', client)
+  }
+
   return (
-    <Button>
+    <ApolloConsumer>
+    {client => (
+    <Button onClick={() => handleSignout(client)}>
       <Typography
         variant="body1"
         className={classes.buttonText}
@@ -16,6 +25,8 @@ const Signout = ({ classes }) => {
       </Typography>
       <ExitToApp className={classes.buttonIcon} color="secondary" />
     </Button>
+    )}
+    </ApolloConsumer>
   )
 };
 
